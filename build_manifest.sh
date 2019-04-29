3748de9011b011f03979f4424d5f3058675f6920
mikenye <mike.nye@xpanse.com.au>
2019-04-29 22:16:54 +0800

    modified:   build_manifest.sh

--- build_manifest.sh
+++ build_manifest.sh
@@ -7,3 +7,9 @@ docker manifest create mikenye/piaware:3.6.3 mikenye/piaware:3.6.3-amd64 mikenye
 docker manifest annotate mikenye/piaware:3.6.3 mikenye/piaware:3.6.3-amd64 --os linux --arch amd64
 docker manifest annotate mikenye/piaware:3.6.3 mikenye/piaware:3.6.3-arm32v7 --os linux --arch arm --variant v7
 docker manifest push mikenye/piaware:3.6.3
+
+docker manifest create --amend mikenye/piaware:latest mikenye/piaware:3.6.3-amd64 mikenye/piaware:3.6.3-arm32v7
+docker manifest annotate mikenye/piaware:latest mikenye/piaware:3.6.3-amd64 --os linux --arch amd64
+docker manifest annotate mikenye/piaware:latest mikenye/piaware:3.6.3-arm32v7 --os linux --arch arm --variant v7
+docker manifest push mikenye/piaware:latest
+
