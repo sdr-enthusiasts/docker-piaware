a4319e47b89fb1aab967434d212398f85632cbe1
mikenye <mike.nye@xpanse.com.au>
2019-04-29 16:35:35 +0800

    new file:   Dockerfile.amd64 modified:   Dockerfile.unoptimised modified:   build_manifest.sh new file:   etc/cont-init.d/01-piaware new file:   etc/lighttpd/piaware.lighttpd.conf new file:   etc/services.d/dump1090/log/run new file:   etc/services.d/dump1090/run new file:   etc/services.d/lighttpd/log/run new file:   etc/services.d/lighttpd/run new file:   etc/services.d/piaware/log/run new file:   etc/services.d/piaware/run deleted:    Dockerfile deleted:    entrypoint.sh deleted:    lighttpd_config_50-piaware.conf deleted:    piaware_config

--- build_manifest.sh
+++ build_manifest.sh
@@ -2,7 +2,8 @@
 # Docker architectures
 # https://github.com/docker-library/official-images/blob/a7ad3081aa5f51584653073424217e461b72670a/bashbrew/go/vendor/src/github.com/docker-library/go-dockerlibrary/architecture/oci-platform.go#L14-L25
 #
-docker manifest create mikenye/piaware:latest mikenye/piaware:latest-arm7l mikenye/piaware:latest-x86_64
-docker manifest annotate mikenye/piaware:latest mikenye/piaware:latest-x86_64 --os linux --arch amd64
-docker manifest annotate mikenye/piaware:latest mikenye/piaware:latest-arm7l --os linux --arch arm --variant v7
-docker manifest push mikenye/piaware:latest
+
+docker manifest create mikenye/piaware:3.5.3 mikenye/piaware:3.5.3-amd64 mikenye/piaware:3.5.3-arm32v7
+docker manifest annotate mikenye/piaware:3.5.3 mikenye/piaware:3.5.3-x86_64 --os linux --arch amd64
+docker manifest annotate mikenye/piaware:3.5.3 mikenye/piaware:3.5.3-arm7l --os linux --arch arm --variant v7
+docker manifest push mikenye/piaware:3.5.3
