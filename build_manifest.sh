d27bed7aed49a0a681e8fff82b6917fa20969c3a
mikenye <mike.nye@xpanse.com.au>
2019-04-29 21:36:11 +0800

    modified:   build_manifest.sh

--- build_manifest.sh
+++ build_manifest.sh
@@ -3,7 +3,7 @@
 # https://github.com/docker-library/official-images/blob/a7ad3081aa5f51584653073424217e461b72670a/bashbrew/go/vendor/src/github.com/docker-library/go-dockerlibrary/architecture/oci-platform.go#L14-L25
 #
 
-docker manifest create mikenye/piaware:3.5.3 mikenye/piaware:3.5.3-amd64 mikenye/piaware:3.5.3-arm32v7
-docker manifest annotate mikenye/piaware:3.5.3 mikenye/piaware:3.5.3-x86_64 --os linux --arch amd64
-docker manifest annotate mikenye/piaware:3.5.3 mikenye/piaware:3.5.3-arm7l --os linux --arch arm --variant v7
-docker manifest push mikenye/piaware:3.5.3
+docker manifest create mikenye/piaware:3.6.3 mikenye/piaware:3.6.3-amd64 mikenye/piaware:3.6.3-arm32v7
+docker manifest annotate mikenye/piaware:3.6.3 mikenye/piaware:3.6.3-amd64 --os linux --arch amd64
+docker manifest annotate mikenye/piaware:3.6.3 mikenye/piaware:3.6.3-arm32v7 --os linux --arch arm --variant v7
+docker manifest push mikenye/piaware:3.6.3
