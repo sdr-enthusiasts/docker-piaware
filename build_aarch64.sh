22e52c09d62075352ed5f4b4354b2360d3eb963b
mikenye <mike.nye@xpanse.com.au>
2019-08-26 16:45:01 +0100

    Add support for arm64v8 (aarch64).
    new file:   Dockerfile.aarch64
    new file:   build_aarch64.sh
    modified:   build_manifest.sh

---
+++ build_aarch64.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+docker build -f Dockerfile.aarch64 -t mikenye/piaware:3.7.1-arm64v8 .
--- build_manifest.sh
+++ build_manifest.sh
@@ -7,14 +7,17 @@ VERSION=3.7.1
 
 docker pull mikenye/piaware:${VERSION}-amd64
 docker pull mikenye/piaware:${VERSION}-arm32v7
+docker pull mikenye/piaware:${VERSION}-arm64v8
 
 docker manifest create --amend mikenye/piaware:${VERSION} mikenye/piaware:${VERSION}-amd64 mikenye/piaware:${VERSION}-arm32v7
 docker manifest annotate mikenye/piaware:${VERSION} mikenye/piaware:${VERSION}-amd64 --os linux --arch amd64
 docker manifest annotate mikenye/piaware:${VERSION} mikenye/piaware:${VERSION}-arm32v7 --os linux --arch arm --variant v7
+docker manifest annotate mikenye/piaware:${VERSION} mikenye/piaware:${VERSION}-arm64v8 --os linux --arch arm64 --variant v8
 docker manifest push --purge mikenye/piaware:${VERSION}
 
 docker manifest create --amend mikenye/piaware:latest mikenye/piaware:${VERSION}-amd64 mikenye/piaware:${VERSION}-arm32v7
 docker manifest annotate mikenye/piaware:latest mikenye/piaware:${VERSION}-amd64 --os linux --arch amd64
 docker manifest annotate mikenye/piaware:latest mikenye/piaware:${VERSION}-arm32v7 --os linux --arch arm --variant v7
+docker manifest annotate mikenye/piaware:latest mikenye/piaware:${VERSION}-arm64v8 --os linux --arch arm64 --variant v8
 docker manifest push --purge mikenye/piaware:latest
 
