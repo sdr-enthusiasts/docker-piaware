175c8ea26b2ff1007a799fa8345acd85af1ae0d1
mikenye <mike.nye@xpanse.com.au>
2020-01-28 14:58:17 +0800

    Add architecture to build scripts. Print piaware version during container init.

--- build_aarch64.sh
+++ build_aarch64.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 VERSION=3.8.0
+ARCH=arm64v8
 IMAGE=mikenye/piaware
 
-docker build -f Dockerfile -t ${IMAGE}:${VERSION} .
+docker build -f Dockerfile -t ${IMAGE}:${VERSION}-${ARCH} .
--- build_armv7l.sh
+++ build_armv7l.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 VERSION=3.8.0
+ARCH=arm32v7
 IMAGE=mikenye/piaware
 
-docker build -f Dockerfile -t ${IMAGE}:${VERSION} .
+docker build -f Dockerfile -t ${IMAGE}:${VERSION}-${ARCH} .
