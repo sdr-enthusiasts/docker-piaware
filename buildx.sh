27e3e86d35ec6620d1502c8d3e11464ae8c3cba9
mikenye <mike.nye@xpanse.com.au>
2020-03-17 22:21:44 +0800

    Update build script

--- buildx.sh
+++ buildx.sh
@@ -7,7 +7,7 @@ IMAGE=piaware
 docker buildx build -t ${REPO}/${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
 
 # Get piaware version from latest
-docker pull mikenye/piaware:latest
+docker pull ${REPO}/${IMAGE}:latest
 VERSION=$(docker run --rm --entrypoint cat ${REPO}/${IMAGE}:latest /VERSIONS | grep piaware | cut -d " " -f 2)
 
 # Build & push version-specific
