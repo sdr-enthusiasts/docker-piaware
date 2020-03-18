aae6b7d9e1ed31aa2c14f530ed0d64e6c6004123
mikenye <mike.nye@xpanse.com.au>
2020-03-18 13:44:22 +0800

    Add docker context and buildx use

--- buildx.sh
+++ buildx.sh
@@ -3,6 +3,10 @@
 REPO=mikenye
 IMAGE=piaware
 
+docker context use x86_64
+export DOCKER_CLI_EXPERIMENTAL="enabled"
+docker buildx use homecluster
+
 # Build & push latest
 docker buildx build -t ${REPO}/${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
 
