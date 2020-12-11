133a618cca069fda91da39a3c3b64096194835dd
Mike Nye <mike.nye@gmail.com>
2020-12-11 20:24:09 +0800

    change from set-env to https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-an-environment-variable

--- .github/get_version.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# Get the latest version of the image
-VERSION_TAG=$(docker run --rm --entrypoint cat "${REPO}/${IMAGE}:latest" /VERSIONS | grep piaware | cut -d " " -f 2)
-
-# Set github action environment variable VERSION_TAG
-echo "::set-env name=VERSION_TAG::$VERSION_TAG"
