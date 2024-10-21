53a362565cbd05c12e34d7ea7311755bec00000d
wiedehopf <wiedehopf@users.noreply.github.com>
2024-10-21 03:27:14 +0200

    978 location / service restart cornercase fixup (#211)
    * skyaware978: use piaware location
    
    * fix docker not being detected in fa_services.tcl
    
    /.dockerenv is not a docker standard don't rely on it
    the container uses a custom fa_services.tcl anyhow, just hardcode
    is_docker to return 1

--- rootfs/usr/lib/piaware_packages/fa_services.tcl
+++ rootfs/usr/lib/piaware_packages/fa_services.tcl
@@ -22,9 +22,9 @@ namespace eval ::fa_services {
 	# return 1 if we have invoke-rc.d
 	proc has_invoke_rcd {} {
                 # Check if we are running on docker because no runlevel exists to use invoke-rc.d and return immediately 0
-		if {[is_docker]} {
-			logger "has_invoke_rcd: Running on docker ignoring invoke-rc.d"
-                        return 0
+        if {[is_docker]} {
+            logger "has_invoke_rcd: Running on docker ignoring invoke-rc.d"
+            return 0
 		}
 
 		if {![info exists ::invoke_rcd_path]} {
@@ -69,7 +69,8 @@ namespace eval ::fa_services {
 
         # return 1 if it looks like we're using docker
         proc is_docker {} {
-		return [file isfile /.dockerenv]
+            return 1
+            #return [file isfile /.dockerenv]
         }
 
 	# return a list of systemd service unitfiles matching pattern
